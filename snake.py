0caaf1a7594c80c89f5d856d8b6b7d70ecdc79ad
Junwon Lee <jwseri1436@gmail.com>
2024-06-04 20:55:13 -0700

    first upload

--- snake.py
+++ snake.py
@@ -1,21 +1,39 @@
-import pygame
-import sys
+import pygame, sys, random
 from pygame.math import Vector2
 
 pygame.init()
 
-GREEN = (173, 204, 96)
-DARK_GREEN = (43, 51, 24)
+GREEN = (179, 225, 172)
+DARK_GREEN = (133, 196, 120)
 
 cell_size = 30
 number_of_cells = 25
 
 class Food:
     def __init__(self):
-        self.position = Vector2(5, 6)
+        # self.position = Vector2(5, 6)
+        self.position = self.generate_random_pos()
+
     def draw(self):
         food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size, cell_size, cell_size)
-        pygame.draw.rect(screen, DARK_GREEN, food_rect)
+        #pygame.draw.rect(screen, DARK_GREEN, food_rect)
+        screen.blit(food_surface, food_rect)
+ 
+    def generate_random_pos(self):
+        x = random.randint(0, number_of_cells -1)
+        y = random.randint(0, number_of_cells -1)
+        position = Vector2(x, y)
+        return position
+
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(6, 9), Vector2(5, 9), Vector2(4, 9)]
+        self.direction = Vector2(1, 0)
+
+    def draw(self):
+        for segment in self.body:
+            segment_rect = (segment.x * cell_size, segment.y * cell_size, cell_size, cell_size)
+            pygame.draw.rect(screen, DARK_GREEN, segment_rect, 0, 7)
 
 screen = pygame.display.set_mode((cell_size * number_of_cells, cell_size * number_of_cells))
 
@@ -24,6 +42,8 @@ pygame.display.set_caption("Retro Snake")
 clock = pygame.time.Clock()
 
 food = Food()
+snake = Snake()
+food_surface = pygame.image.load("Graphics/food.png")
 
 while True:
     for event in pygame.event.get():
@@ -34,6 +54,7 @@ while True:
     # Drawing
     screen.fill(GREEN)
     food.draw()
+    snake.draw()
 
     pygame.display.update()
     clock.tick(60)
