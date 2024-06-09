d291bec94047a259d61166cfb343a89222ec69af
Junwon Lee <jwseri1436@gmail.com>
2024-06-09 15:49:19 -0700

    Changes

--- snake.py
+++ snake.py
@@ -10,19 +10,25 @@ cell_size = 30
 number_of_cells = 25
 
 class Food:
-    def __init__(self):
+    def __init__(self, snake_body):
         # self.position = Vector2(5, 6)
-        self.position = self.generate_random_pos()
+        self.position = self.generate_random_pos(snake_body)
 
     def draw(self):
         food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size, cell_size, cell_size)
-        #pygame.draw.rect(screen, DARK_GREEN, food_rect)
+        # pygame.draw.rect(screen, DARK_GREEN, food_rect)
         screen.blit(food_surface, food_rect)
  
-    def generate_random_pos(self):
+    def generate_random_cell(self):
         x = random.randint(0, number_of_cells -1)
         y = random.randint(0, number_of_cells -1)
-        position = Vector2(x, y)
+        return Vector2(x, y)
+
+
+    def generate_random_pos(self, snake_body):
+        position = self.generate_random_cell()
+        while position in snake_body:
+            position = self.generate_random_cell()
         return position
 
 class Snake:
@@ -35,26 +41,67 @@ class Snake:
             segment_rect = (segment.x * cell_size, segment.y * cell_size, cell_size, cell_size)
             pygame.draw.rect(screen, DARK_GREEN, segment_rect, 0, 7)
 
+    def update(self):
+        self.body = self.body[:-1]
+        self.body.insert(0, self.body[0] + self.direction)
+
+class Game:
+    def __init__(self):
+        self.snake = Snake()
+        self.food = Food(self.snake.body)
+    
+    def draw(self):
+        self.food.draw()
+        self.snake.draw()
+
+    def update(self):
+        self.snake.update()
+        self.check_collision_with_food()
+
+    def check_collision_with_food(self):
+        if self.snake.body[0] == self.food.position:
+            self.food.position = self.food.generate_random_pos(self.snake.body)
+            # print("Eating Food")
+
 screen = pygame.display.set_mode((cell_size * number_of_cells, cell_size * number_of_cells))
 
 pygame.display.set_caption("Retro Snake")
 
 clock = pygame.time.Clock()
 
-food = Food()
-snake = Snake()
+# food = Food()
+# snake = Snake()
+game = Game()
 food_surface = pygame.image.load("Graphics/food.png")
+
+SNAKE_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SNAKE_UPDATE, 200)
 
 while True:
     for event in pygame.event.get():
+        if event.type == SNAKE_UPDATE:
+            game.update()
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and game.snake.direction != Vector2(0, 1):
+                game.snake.direction = Vector2(0, -1)
+            if event.key == pygame.K_DOWN and game.snake.direction != Vector2(0, -1):
+                game.snake.direction = Vector2(0, 1)
+            if event.key == pygame.K_LEFT and game.snake.direction != Vector2(1, 0):
+                game.snake.direction = Vector2(-1, 0)
+            if event.key == pygame.K_RIGHT and game.snake.direction != Vector2(-1, 0):
+                game.snake.direction = Vector2(1, 0)
+
+    # snake.update()
+
     # Drawing
     screen.fill(GREEN)
-    food.draw()
-    snake.draw()
+    game.draw()
+    # food.draw()
+    # snake.draw()
 
     pygame.display.update()
     clock.tick(60)
